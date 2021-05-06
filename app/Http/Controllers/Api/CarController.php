72bfdd65ac0f6eba15b27f220b7c8741904f67be
acver <1246009411@qq.com>
2021-05-06 12:21:07 +0800

    接口文档注释

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -9,6 +9,56 @@ use Illuminate\Http\Request;
 class CarController extends Controller
 {
     //
+    /**
+     * @api {get} /api/cars/:id     汽车列表
+     * @apiGroup 车辆
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 汽车列表
+     *
+     * @apiParam {Number}  page        分页页码，从1开始
+     * @apiParam {Number}  pre_page    每页记录条数
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     车辆列表
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     * "code":0,
+     * "status":"成功",
+     * "data":[
+     *     {
+     *         "id":1,
+     *         "model":1,
+     *         "positionX":"122.57667",
+     *         "positionY":"31.202186",
+     *         "mileage":"10",
+     *         "number_plate":"沪B00001",
+     *         "imageUrls":"[]",
+     *         "created_at":"2021-04-25 00:00:00",
+     *         "updated_at":"2021-04-25 00:00:00",
+     *         "model_name":"比亚迪汉EV",
+     *         "model_img":"https://p4.itc.cn/images01/20210204/328dd9f36e614232ba44e5ac59c2fd89.jpeg",
+     *         "brand":"比亚迪"
+     *     },
+     *     {
+     *         "id":2,
+     *         "model":2,
+     *         "positionX":"121.47667",
+     *         "positionY":"31.202184",
+     *         "mileage":"300",
+     *         "number_plate":"沪A00001",
+     *         "imageUrls":"[]",
+     *         "created_at":"2021-04-28 00:00:00",
+     *         "updated_at":"2021-04-28 00:00:00",
+     *         "model_name":"宝马三系",
+     *         "model_img":"https://car2.autoimg.cn/cardfs/product/g30/M06/FD/CA/1024x0_1_q95_autohomecar__ChsEf2AbnlGAXWcNAC15oG1haCc768.jpg",
+     *         "brand":"宝马"
+     *     }
+     * ]
+     *}     
+     */
     public function getList(Request $request) {
         $data = $request->only(["page", "pre_page"]);
         return msg(0, Car::list($data['page'], $data['pre_page']));
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,15 +9,49 @@ use App\User;
 class UserController extends Controller
 {
     //
+    /**
+     * @api {post} /user     注册
+     * @apiGroup 用户
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 注册
+     *
+     * @apiParam {String}  account     帐号
+     * @apiParam {String}  password    密码
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     后端参考信息，前端无关
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {"code":0,"status":"成功","data":43}
+     */
     public function register(Request $request) {
         $data = $request->only(["account", "password"]);
         $rel = User::register($data["account"], $data["password"]);
         if(!$rel) {
-            return msg(-1, "失败");
+            return msg(-1, __LINE__);
         }
-        return msg(0, "成功");
+        return msg(0, __LINE__);
     }
 
+    /**
+     * @api {get} /user     登陆
+     * @apiGroup 用户
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 登陆
+     *
+     * @apiParam {String}  account     帐号
+     * @apiParam {String}  password    密码
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     后端参考信息，前端无关
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {"code":0,"status":"成功","data":43}
+     */
     public function login(Request $request) {
         $data = $request->only(["account", "password"]);
         $user = User::query()->where('account', $data["account"])->first();
@@ -25,9 +59,26 @@ class UserController extends Controller
             session(["uid" => $user->id]);
             return msg(0, $user->info());
         }
-        return msg(2, "帐号密码不匹配");
+        return msg(2, "帐号密码不匹配" . __LINE__);
     }
 
+    /**
+     * @api {post} /user/password     修改密码
+     * @apiGroup 用户
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 修改密码
+     *
+     * @apiParam {String}  password     密码
+     * @apiParam {String}  new_password    新密码
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     后端参考信息，前端无关
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {"code":0,"status":"成功","data":43}
+     */
     public function updatePassword(Request $request) {
         $data = $request->only(["password", "new_password"]);
         $user = User::find(session('uid'));
@@ -35,19 +86,51 @@ class UserController extends Controller
         return msg(0, "");
     }
 
+    /**
+     * @api {post} /user/signature     修改个性签名
+     * @apiGroup 用户
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 修改个性签名
+     *
+     * @apiParam {String}  signature     个性签名
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     后端参考信息，前端无关
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {"code":0,"status":"成功","data":43}
+     */
     public function updateSignature(Request $request) {
         $data = $request->only(["signature"]); 
         $user = User::find(session('uid'));
         $user->signature = $data["signature"];
         $user->save();
-        return msg(0, "");
+        return msg(0, __LINE__);
     }
 
+    /**
+     * @api {post} /user/signature     修改头像
+     * @apiGroup 用户
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription 修改头像
+     *
+     * @apiParam {String}  signature     头像url
+     *
+     * @apiSuccess {Number} code     状态码，0：请求成功
+     * @apiSuccess {String} message  提示信息
+     * @apiSuccess {Object} data     后端参考信息，前端无关
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {"code":0,"status":"成功","data":43}
+     */
     public function updateAvatar(Request $request) {
         $data = $request->only(["avatar"]);
         $user = User::find(session('uid'));
         $user->avatar = $data["avatar"];
         $user->save();
-        return msg(0, "");
+        return msg(0, __LINE__);
     }
 }
